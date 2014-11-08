25a8049a899f82fa8801631c46998ae4df518656
Alok Shankar <alok.shankar.m@gmail.com>
2014-11-08 19:59:58 +0530

    crazy sorting

--- app/controllers/analyze_controller.rb
+++ app/controllers/analyze_controller.rb
@@ -2,19 +2,7 @@ class AnalyzeController < ApplicationController
 	def index
 		text = params[:text]
 		sentences = text.split(/[.?!]/)
-		disputes = Dispute.all
-		match = nil
-		max = 0
-		disputes.each do |dispute|
-		mnum = (dispute.policy_text.split(/[.?!]/) & sentences).count
-			if( mnum >  max)
-				match = dispute.policy_name
-				max = mnum
-			end
-		end
-
-
-
-		render :json => match
+		disputes = Dispute.all.sort{ |x, y| (x.policy_text.split(/[.?!]/) & sentences).count <=> (y.policy_text.split(/[.?!]/) & sentences).count }
+		render :json => disputes
 	end
 end
