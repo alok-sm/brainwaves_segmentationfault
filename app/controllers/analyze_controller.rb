7e43b77ef6023ba3c6a97ba5e439fff1ee22f4a2
Alok Shankar <alok.shankar.m@gmail.com>
2014-11-08 20:54:34 +0530

    basic highlighting

--- app/controllers/analyze_controller.rb
+++ app/controllers/analyze_controller.rb
@@ -3,6 +3,16 @@ class AnalyzeController < ApplicationController
 		text = params[:text]
 		sentences = text.split(/[.?!]/)
 		disputes = Dispute.all.sort{ |x, y| -((x.policy_text.split(/[.?!]/) & sentences).count <=> (y.policy_text.split(/[.?!]/) & sentences).count )}.first(5)
-		render :json => disputes
+		rendertext = disputes.policy_text
+		clauses = []
+		disputes.each do |d|
+			clauses << d.claim_text
+		end
+
+		clauses.each do |c|
+			rendertext.gsub(rendertext, '<mark style = "background-color: red">'+rendertext+'</mark>')
+		end
+		
+		render :inline => rendertext
 	end
 end
