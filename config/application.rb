f002cd4b760fefc03d9c9d7bbb29f8552f50179a
hybridNeo <rahul.mahadev7@gmail.com>
2014-11-08 22:10:42 +0530

    Another

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Preview
   class Application < Rails::Application
+    config.action_dispatch.default_headers.merge!({
+  'Access-Control-Allow-Origin' => '*',
+  'Access-Control-Allow-Methods' => 'GET,POST,PUT,DELETE',
+  'Access-Control-Request-Method' => '*'
+})
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
