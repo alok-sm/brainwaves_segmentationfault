ed5a5c297519c5fd337279a35612a1b9cb558987
hybridNeo <rahul.mahadev7@gmail.com>
2014-11-08 20:05:13 +0530

    Sorting jai

--- app/controllers/analyze_controller.rb
+++ app/controllers/analyze_controller.rb
@@ -2,7 +2,7 @@ class AnalyzeController < ApplicationController
 	def index
 		text = params[:text]
 		sentences = text.split(/[.?!]/)
-		disputes = Dispute.all.sort{ |x, y| (x.policy_text.split(/[.?!]/) & sentences).count <=> (y.policy_text.split(/[.?!]/) & sentences).count }.first(5)
+		disputes = Dispute.all.sort{ |x, y| -((x.policy_text.split(/[.?!]/) & sentences).count <=> (y.policy_text.split(/[.?!]/) & sentences).count )}.first(1)
 		render :json => disputes
 	end
 end
