9646b11c59592c8845f04d788818aea87aa77d5b
Alok Shankar <alok.shankar.m@gmail.com>
2014-11-08 23:21:58 +0530

    partial paperclip

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem "paperclip", "~> 4.2"
 require 'thread'
 gem 'thin'
 gem 'googlecharts'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108153129) do
+ActiveRecord::Schema.define(version: 20141108174824) do
 
   create_table "clauses", force: true do |t|
     t.integer  "dispute_id"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20141108153129) do
     t.string   "policy_link"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "uploads", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
   end
 
 end
