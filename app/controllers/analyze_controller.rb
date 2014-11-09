d94f0f648f61a8c13f53906e596d517a17c580ee
Alok Shankar <alok.shankar.m@gmail.com>
2014-11-09 05:57:53 +0530

    habeus corpus

--- app/controllers/analyze_controller.rb
+++ app/controllers/analyze_controller.rb
@@ -27,13 +27,21 @@ class AnalyzeController < ApplicationController
 		# 		clauses << cl.clause_text
 		# 	end
 		# end
+		writs = {
+			'habeus corpus'=> 'To have a body',
+			'certiorari'=> 'a writ seeking judicial review. It is issued by a superior court, directing an inferior court, tribunal, or other public authority to send the record of a proceeding for review.',
+			'mandamus'=> 'Mandamus is a judicial remedy in the form of an order from a superior court, to any government subordinate court, corporation, or public authority—to do (or forbear from doing) some specific act which that body is obliged under law to do (or refrain from doing)—and which is in the nature of public duty, and in certain cases one of a statutory duty.',
+			'procedendo'=> 'It is a writ that sends a case from an appellate court to a lower court with an order to proceed to judgment.',
+			'writ of prohibition'=> 'A writ of prohibition is a writ directing a subordinate to stop doing something the law prohibits.',
+			'quo warranto'=> 'Quo warranto (Medieval Latin for "by what warrant?") is a prerogative writ requiring the person to whom it is directed to show what authority they have for exercising some right or power (or "franchise") they claim to hold.'
+		}
 		puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
 		clauses.each do |c|
 			puts c.clause_text
 			if dispute.policy_link != ''
 				text = text.gsub(c.clause_text, '<a href = "'+
 					dispute.policy_link+
-					'"><mark style = "background-color:'+
+					'" target="_blank"><mark style = "background-color:'+
 					c.clause_status+'">'+
 					c.clause_text+
 					'</mark></a>')
@@ -46,8 +54,20 @@ class AnalyzeController < ApplicationController
 				puts text
 			end			
 		end
+
+		writs.keys.each do |w|
+			text = text.gsub(w, 
+				'<button type="button" class="btn btn-default" data-toggle="popover" data-placement="top" title="'+
+				writs[w]+
+				'">'+
+				w+
+				'</button>'
+
+			)
+		end
 		puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
 		# raise 'a'
+		sleep(3)
 		render :inline => text
 	end
 end
